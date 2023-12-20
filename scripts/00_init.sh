075d4b7a9a5a84643532c419e82dca1c86cd9141
Michael Theis (msg) <michael.theis@msg.group>
2023-12-20 15:09:39 +0100

    Improved docs in all scripts; removed obsolete installation of AWS CLI v2

--- scripts/00_init.sh
+++ scripts/00_init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# ----------------------------------------------------------------------------
+# 00_init.sh
+# ----------------------------------------------------------------------------
+# Performs some basic initialization steps like updating all packages etc.
+# ----------------------------------------------------------------------------
 
 echo 'updating installed packages'
 sudo dnf update -y
